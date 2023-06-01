75b98990a2fd7f7358b885bd7033b0a45d49d9bb
EmmanuelAkinyemi <93402402+EmmanuelAkinyemi@users.noreply.github.com>
2023-06-01 08:18:05 -0700

    correction almost at the finish line

--- resources/views/admin/team.blade.php
+++ resources/views/admin/team.blade.php
@@ -9,5 +9,32 @@
         </div>
         <a href="create-property" class="btn btn-primary rounded"><i class="fas fa-plus me-0"></i></a>
     </div>
+
+    <div class="row">
+        <div class="col-xl-3 col-lg-6 col-md-4 col-sm-6">
+            <div class="card">
+                <div class="card-body">
+                    <div class="new-arrival-product">
+                        <div class="new-arrivals-img-contnent">
+                            <img class="img-fluid w-100 h-75 relative" src="{{ asset("admin/images/profile/pic1.jpg") }}" alt="">
+                        </div>
+                        <div class="new-arrival-content text-center mt-3">
+                            <h4>James Allison</h4>
+                            <p><span class="text-small fw-light">Operating Director</span></p>
+                            <div class="row clearfix">
+                                <div class="col-6">
+                                    <li><a href=""><i class="fa fa-pen text-success"></i></a></li>
+                                </div>
+                                <div class="col-6">
+                                    <li><a href=""><i class="fa fa-trash text-danger"></i></a></li>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </div>
+    </div>
 </div>
 @endsection
